9e809a4d7bec321814de9c140323889c1fbdc423
Justin Bennett <zephraph@gmail.com>
2019-09-14 10:31:27 -0400

    Setup new sdk & cli

--- packages/sdk/src/models/issue.generated.ts
+++ packages/sdk/src/models/issue.generated.ts
@@ -1,5 +1,52 @@
 import * as Types from '../schema';
 
+export type IssueDetailsFragment = (
+  { __typename?: 'Issue' }
+  & Pick<Types.Issue, 'id' | 'title' | 'number' | 'description' | 'priority' | 'estimate' | 'assignee'>
+  & { labels: Array<(
+    { __typename?: 'IssueLabel' }
+    & Pick<Types.IssueLabel, 'id' | 'name'>
+  )> }
+);
+
+export type GetAllIssuesFromCliQueryVariables = {};
+
+
+export type GetAllIssuesFromCliQuery = (
+  { __typename?: 'Query' }
+  & { issues: Array<{ __typename?: 'Issue' }
+    & IssueDetailsFragment
+  > }
+);
+
+export type GetIssueFromCliQueryVariables = {
+  id: Types.Scalars['String']
+};
+
+
+export type GetIssueFromCliQuery = (
+  { __typename?: 'Query' }
+  & { issue: { __typename?: 'Issue' }
+    & IssueDetailsFragment
+   }
+);
+
+export type GetTeamIssueIdsFromCliQueryVariables = {
+  teamId: Types.Scalars['String']
+};
+
+
+export type GetTeamIssueIdsFromCliQuery = (
+  { __typename?: 'Query' }
+  & { team: (
+    { __typename?: 'Team' }
+    & { issues: Array<(
+      { __typename?: 'Issue' }
+      & Pick<Types.Issue, 'id' | 'number'>
+    )> }
+  ) }
+);
+
 export type CreateIssueFromCliMutationVariables = {
   title: Types.Scalars['String'],
   teamId: Types.Scalars['String'],
@@ -25,7 +72,8 @@ export type CreateIssueFromCliMutation = (
 export type UpdateIssueFromCliMutationVariables = {
   id: Types.Scalars['String'],
   title?: Types.Maybe<Types.Scalars['String']>,
-  description?: Types.Maybe<Types.Scalars['String']>
+  description?: Types.Maybe<Types.Scalars['String']>,
+  labelIds?: Types.Maybe<Array<Types.Scalars['String']>>
 };
 
 
--- packages/sdk/src/models/issue.ts
+++ packages/sdk/src/models/issue.ts
@@ -1,31 +1,112 @@
 // tslint:disable:no-invalid-this
 import { gql } from "gql";
-import { Linear } from "linear";
+import { Linear } from "../linear";
 import {
   CreateIssueFromCliMutation,
   CreateIssueFromCliMutationVariables,
   UpdateIssueFromCliMutation,
   UpdateIssueFromCliMutationVariables,
+  GetTeamIssueIdsFromCliQuery,
+  GetTeamIssueIdsFromCliQueryVariables,
+  GetIssueFromCliQuery,
+  GetIssueFromCliQueryVariables,
 } from "./issue.generated";
+import { getTeamIdFromKey } from "./team";
 
-type IssueSelection =
-  | {
-      /** UUID of the issue */
-      id: string;
+/**
+ * An object containing the UUID of an issue or an object containing the issue key.
+ * The issue key is formatted like `LNR-123`
+ */
+export type IssueSelection = { id: string } | { key: string };
+
+export const IssueDetails = gql`
+  fragment IssueDetails on Issue {
+    id
+    title
+    number
+    description
+    priority
+    estimate
+    assignee
+    labels {
+      id
+      name
     }
-  | {
-      /**
-       * In the format of `ABC-123`, the issue key is made up of the team key plus the issue number.
-       * This can be found in the URL or in the issue view.
-       **/
-      key: string;
-    };
+  }
+`;
 
-export const selectIssue = async (client: Linear, selection: IssueSelection) => {
-  // Find team
+export const getAllIssues = async (client: Linear) =>
+  client.request(gql`
+    ${IssueDetails}
+    query GetAllIssuesFromCLI {
+      issues {
+        ...IssueDetails
+      }
+    }
+  `);
+
+export const getIssue = async (client: Linear, selection: IssueSelection) => {
+  if ("id" in selection) {
+    return client.request<GetIssueFromCliQuery, GetIssueFromCliQueryVariables>(
+      gql`
+        ${IssueDetails}
+        query GetIssueFromCLI($id: String!) {
+          issue(id: $id) {
+            ...IssueDetails
+          }
+        }
+      `,
+      selection
+    );
+  } else {
+    return getIssueIdFromKey(client, selection.key);
+  }
 };
 
-export const create = async (client: Linear, issueInput: CreateIssueFromCliMutationVariables) =>
+export const getIssueIdFromKey = async (client: Linear, key: string) => {
+  const [teamKey, issueNumber] = key.split("-");
+  const teamId = await getTeamIdFromKey(client, teamKey);
+  if (!teamId) {
+    throw new Error(`No team with the key ${teamKey}`);
+  }
+  const { team } = await client.request<GetTeamIssueIdsFromCliQuery, GetTeamIssueIdsFromCliQueryVariables>(
+    gql`
+      query GetTeamIssueIdsFromCLI($teamId: String!) {
+        team(id: $teamId) {
+          issues {
+            id
+            number
+          }
+        }
+      }
+    `,
+    { teamId }
+  );
+  const issue = team.issues.find(issue => issue.number === parseInt(issueNumber));
+  if (issue) {
+    return issue.id;
+  } else {
+    return null;
+  }
+};
+
+export const getIssueIdFromSelection = (client: Linear, selection: IssueSelection) => {
+  if ("id" in selection) {
+    return Promise.resolve(selection.id);
+  }
+  return getIssueIdFromKey(client, selection.key);
+};
+
+export const getIssueByKey = async (client: Linear, key: string) => {
+  const id = await getIssueIdFromKey(client, key);
+  if (id) {
+    return getIssue(client, { id });
+  } else {
+    return null;
+  }
+};
+
+export const createIssue = async (client: Linear, issueInput: CreateIssueFromCliMutationVariables) =>
   client.request<CreateIssueFromCliMutation, CreateIssueFromCliMutationVariables>(
     gql`
       mutation CreateIssueFromCLI($title: String!, $teamId: String!, $labelIds: [String!]) {
@@ -43,10 +124,10 @@ export const create = async (client: Linear, issueInput: CreateIssueFromCliMutat
     issueInput
   );
 
-export const update = async (client: Linear, issueInput: UpdateIssueFromCliMutationVariables) =>
+export const updateIssue = async (client: Linear, issueInput: UpdateIssueFromCliMutationVariables) =>
   client.request<UpdateIssueFromCliMutation, UpdateIssueFromCliMutationVariables>(
     gql`
-      mutation UpdateIssueFromCLI($id: String!, $title: String, $description: String) {
+      mutation UpdateIssueFromCLI($id: String!, $title: String, $description: String, $labelIds: [String!]) {
         issueUpdate(id: $id, input: { title: $title, description: $description }) {
           issue {
             id
@@ -61,10 +142,10 @@ export const update = async (client: Linear, issueInput: UpdateIssueFromCliMutat
     issueInput
   );
 
-// type LabelInput = { name: string }
-// export const addLabel = async (client: Linear, )
-
 export const issue = (client: Linear) => ({
-  create: create.bind(null, client),
-  // get(issue: IssueSelection) { }
+  create: createIssue.bind(null, client),
+  get: getIssue.bind(null, client),
+  getAll: getAllIssues.bind(null, client),
+  getIdFromKey: getIssueByKey.bind(null, client),
+  getByKey: getIssueByKey.bind(null, client),
 });
